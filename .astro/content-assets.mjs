589395fff19c76ac7ac4e74f0e2e15909c0cd60b
Josh Hudson <josh@bamboonine.co.uk>
2026-03-26 08:44:37 +0000

    text image resize

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_ZfXoPA from "./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world-again.md";
-import __ASTRO_IMAGE_IMPORT_Z2eba9q from "./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md";
-import __ASTRO_IMAGE_IMPORT_BKsem from "./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthird-post.md";
-export default new Map([["./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fhello-world-again.md", __ASTRO_IMAGE_IMPORT_ZfXoPA], ["./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md", __ASTRO_IMAGE_IMPORT_Z2eba9q], ["./img/profile.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthird-post.md", __ASTRO_IMAGE_IMPORT_BKsem]]);
-		
+export default new Map();
--- src/content/blog/test-images.md
+++
@@ -1,9 +0,0 @@
----
-title: "test images"
-description: "this is an image test"
-pubDate: 2026-03-26T07:57:00Z
-draft: false
-heroImage: "./img/test-images-1774511898048.jpg"
-tags: ["Thought"]
----
-test image text 
